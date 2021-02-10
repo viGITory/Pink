262c4382ccf34eba57850ba0a1398325e7197746
PANTORY <pantory@gmail.com>
2021-02-11 00:11:21 +0300

    minor fixes

--- blocks/button-burger/button-burger.js
+++ blocks/button-burger/button-burger.js
@@ -1,13 +1,13 @@
 export const toggleBurger = () => {
 	let btnBurger = document.querySelector('.button-burger');
-	let btnBurgerIcon = document.querySelector('.button-burger__icon');
-	let btnBurgerCross = document.querySelector('.button-burger__icon-cross');
+	let btnBurgerIconBurger = document.querySelector('.button-burger__icon-burger');
+	let btnBurgerIconCross = document.querySelector('.button-burger__icon-cross');
 	let mainHeaderNav = document.querySelector('.main-header__nav');
 	let mainHeaderMenu = document.querySelector('.main-header__menu');
 
 	btnBurger.addEventListener('click', () => {
-		btnBurgerIcon.classList.toggle('js-btn-burger__icon--hide');
-		btnBurgerCross.classList.toggle('js-btn-burger__cross--show');
+		btnBurgerIconBurger.classList.toggle('js-btn-burger__icon--hide');
+		btnBurgerIconCross.classList.toggle('js-btn-burger__cross--show');
 		mainHeaderNav.classList.toggle('js-main-header__nav--bg');
 		mainHeaderMenu.classList.toggle('js-main-header__menu--show');
 	});
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,2 @@
 import { toggleBurger } from '../blocks/button-burger/button-burger.js';
-import { togglePrice } from '../blocks/price/price.js';
+// import { togglePrice } from '../blocks/price/price.js';
